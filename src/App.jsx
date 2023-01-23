2829d0a6113002449771dd3b1610947f67747eb3
DrizzleMoelay <moemoehtay2025@gmail.com>
2023-01-23 11:39:06 +0000

    初期設定&コンポーネント作成

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.css";
+import { InputText } from "./components/InputText";
+import { MemorizationText } from "./components/MemorizationText";
+import { NewvocabText } from "./components/NewvocabText";
+
+export const App = () => {
+  return null;
+};
