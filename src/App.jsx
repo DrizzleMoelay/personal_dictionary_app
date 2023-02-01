494acf51c5c7003631de04dec8bfaccff07aaa34
DrizzleMoelay <moemoehtay2025@gmail.com>
2023-02-01 01:16:41 +0000

    3つのエリア作成

--- src/App.jsx
+++ src/App.jsx
@@ -5,5 +5,32 @@ import { MemorizationText } from "./components/MemorizationText";
 import { NewvocabText } from "./components/NewvocabText";
 
 export const App = () => {
-  return null;
+  return (
+    <>
+      <div>
+        <input placeholder="語彙を入力" />
+        <input placeholder="解説を入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>新語彙</p>
+        <ul>
+          <div>
+            <li>aaa</li>
+            <button>暗記済</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>暗記済語彙</p>
+        <ul>
+          <div>
+            <li>aaa</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
